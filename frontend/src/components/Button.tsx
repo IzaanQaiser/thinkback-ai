902689be0b089e5e409631a6c84ca996c84ab014
Izaan Qaiser <izaanqaiser05@gmail.com>
2025-07-07 18:05:26 -0400

    fixing backend

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
   variant?: 'primary' | 'secondary' | 'danger' | 'ghost' | 'login';
   disabled?: boolean;
 };
-// test4
+// test5
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
