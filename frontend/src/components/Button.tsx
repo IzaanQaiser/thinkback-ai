fd4c1a8ae6f3f6d6083c2de556c13e2cacf7f7cc
Izaan Qaiser <izaanqaiser05@gmail.com>
2025-07-07 15:15:40 -0400

    backedn gh actions setup3

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
   variant?: 'primary' | 'secondary' | 'danger' | 'ghost' | 'login';
   disabled?: boolean;
 };
-// test
+// test1
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
