c3a6040e5d28ea41bea2f4bb1f193ac8c84fe44a
Izaan Qaiser <izaanqaiser05@gmail.com>
2025-07-07 12:53:15 -0400

    triggering a build

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
   variant?: 'primary' | 'secondary' | 'danger' | 'ghost' | 'login';
   disabled?: boolean;
 };
-
+// test
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
